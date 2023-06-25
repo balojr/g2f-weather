079d81e35df9c32a19f326689695fa12ad19244b
balojr <77960542+balojr@users.noreply.github.com>
2023-06-25 23:06:55 +0300

    add api and url

--- weather/src/app/services/weather.service.ts
+++ weather/src/app/services/weather.service.ts
@@ -1,23 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  private apiKey = 'ca3cba9313924bedba770304232206';
-  private apiUrl = 'https://api.weatherapi.com/v1/future.json';
+  // private apiKey = 'ca3cba9313924bedba770304232206';
+  private apiUrl = 'https://api.weatherapi.com/v1/future.json?key=ca3cba9313924bedba770304232206&q=Nairobi, Kenya&dt=2023-07-25';
 
-  constructor(private http: HttpClient) { }
+  constructor(private https: HttpClient) { }
 
   // getWeather(city: string, days: number) {
   //   return this.http.get(`${this.apiUrl}?key=${this.apiKey}&q=${city}&days=${days}`);
   // }
-  getWeatherData(city: string, date: string) {
-    const url = `${this.apiUrl}?key=${this.apiKey}&q=${city}&dt=${date}`;
-    return this.http.get(url)
-      .toPromise()
-      .catch(error => console.error('Error fetching weather data:', error));
+  getWeatherData(): Observable<any> {
+    // const url = `${this.apiUrl}?key=${this.apiKey}&q=${city}%20&dt=${localtime}n`;
+    return this.https.get(this.apiUrl).pipe(
+      catchError(error => {
+        console.error('Error fetching weather data:', error);
+        return throwError('Something went wrong.');
+      })
+    );
   }
 }
