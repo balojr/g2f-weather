ca27cbdbaa21bc57d76707722b3361479cfd12d7
balojr <77960542+balojr@users.noreply.github.com>
2023-06-25 23:07:30 +0300

    add forecast logic

--- weather/src/app/home/weatherdisplay/weatherdisplay.component.ts
+++ weather/src/app/home/weatherdisplay/weatherdisplay.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
   selector: 'app-weatherdisplay',
   templateUrl: './weatherdisplay.component.html',
   styleUrls: ['./weatherdisplay.component.scss']
 })
-export class WeatherdisplayComponent {
+export class WeatherdisplayComponent implements OnInit{
+  forecastData: any;
+  hours: any;
+
+  date = Date;
+
+  thedate = new Date();
+  // the seconds should reflect in real time
+
+  dayImg = '/../assets/icons/fluent-emoji_face-in-clouds.svg';
+  tempimg = '/../assets/icons/fluent-temperature-24-regular.svg';
+
+
+  constructor(private weatherService: WeatherService) {
+    this.thedate = new Date();
+    setInterval(() => {
+    this.thedate = new Date();
+    }, 1000);
+   }
+
+  ngOnInit(): void {
+
+    this.weatherService.getWeatherData()
+    .subscribe(
+      data => {
+        this.forecastData = data;
+        console.log(data);
+      },
+      error => {
+        console.error('Error fetching weather data:', error);
+      }
+    );
+  }
 
 }
