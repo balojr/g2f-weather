8e5f263bdd1bfb0038b8cb0608a8368a382f9afe
balojr <77960542+balojr@users.noreply.github.com>
2023-06-26 11:02:57 +0300

    add updated routing

--- weather/src/app/app-routing.module.ts
+++ weather/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WeatherdisplayComponent} from "./home/weatherdisplay/weatherdisplay.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/weather', pathMatch: 'full' },
+  { path: 'weather', component: WeatherdisplayComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
